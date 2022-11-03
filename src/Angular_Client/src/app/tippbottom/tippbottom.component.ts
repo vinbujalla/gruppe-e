654fff9542de174ece0e53577ec3af21a8f1d8a6
Vincent <vincent.bujalla@stud.uni-due.de>
2022-11-03 17:29:56 +0100

    Student in User umbenannt

--- src/Angular_Client/src/app/tippbottom/tippbottom.component.ts
+++ src/Angular_Client/src/app/tippbottom/tippbottom.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sepbottom',
+  selector: 'app-tippbottom',
   templateUrl: './tippbottom.component.html',
   styleUrls: ['./tippbottom.component.scss']
 })
