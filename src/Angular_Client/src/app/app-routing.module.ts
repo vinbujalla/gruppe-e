8aa822c6a2d43127a469c950be9ec5fc1a602d76
Vincent <vincent.bujalla@stud.uni-due.de>
2022-11-06 21:03:17 +0100

    Registrierung und 2-Faktor Oberfläche fertig

--- src/Angular_Client/src/app/app-routing.module.ts
+++ src/Angular_Client/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {EnvironmentInjector, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserListComponent} from "./user-list/user-list/user-list.component";
 import {UserSearchComponent} from "./user-search/user-search.component";
@@ -7,6 +7,7 @@ import {RegisterFormComponent} from "./register-form/register-form.component";
 import {FaktorAuthentifizierungComponent} from "./faktor-authentifizierung/faktor-authentifizierung.component";
 import {CreateLeagueComponent} from "./create-league/create-league.component";
 import {GameScheduleComponent} from "./game-schedule/game-schedule.component";
+import {EditProfileComponent} from "./edit-profile/edit-profile.component";
 
 const routes: Routes = [
   {path:'users',component:UserListComponent},
@@ -14,8 +15,8 @@ const routes: Routes = [
   {path:'register',component:RegisterFormComponent},
   {path:'2-faktor',component:FaktorAuthentifizierungComponent},
   {path: 'create-league',component: CreateLeagueComponent},
-  {path: 'game-schedule', component:GameScheduleComponent}
-
+  {path: 'game-schedule', component:GameScheduleComponent},
+  {path: 'edit-profile', component:EditProfileComponent},
 ];
 
 @NgModule({
